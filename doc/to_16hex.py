e57db8159151ba5d908c30fbef432c9cdfe976cd
ccc179 <ccc179@gmail.com>
2020-05-31 08:00:37 +0800

    更新16进制字符串互转,更新二进制字符串互转

--- doc/to_16hex.py
+++ doc/to_16hex.py
@@ -4,6 +4,12 @@ def str_to_hex(s):
 def hex_to_str(s):
     return ''.join([chr(i) for i in [int(b, 16) for b in s.split(' ')]])
 
+def str_to_bin(s):
+    return ' '.join([bin(ord(c)).replace('0b', '') for c in s])
+
+def bin_to_str(s):
+    return ''.join([chr(i) for i in [int(b, 2) for b in s.split(' ')]])
+
 
 if __name__ == '__main__':
     hex_content = str_to_hex("sdHJH8728HJKHDhjsh67")
